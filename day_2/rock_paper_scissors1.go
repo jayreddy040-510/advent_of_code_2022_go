00cc2168f6afb808072bb7e68a636caecffcbfe1
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-06-18 13:33:21 -0700

    completes initial rock_paper_scissors1.go

--- day_2/rock_paper_scissors1.go
+++ day_2/rock_paper_scissors1.go
@@ -6,8 +6,38 @@ import (
     "strings"
 )
 
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
 func main() {
     
-    // code
+    scoreMap := make(map[string]int)
+    
+    scoreMap["A X"] = 4
+    scoreMap["B Y"] = 5
+    scoreMap["C Z"] = 6
+    scoreMap["A Y"] = 8
+    scoreMap["A Z"] = 3
+    scoreMap["B X"] = 1
+    scoreMap["B Z"] = 9
+    scoreMap["C X"] = 7
+    scoreMap["C Y"] = 2
 
+    data, err := os.ReadFile("puzzle_input.txt")
+
+    check(err)
+
+    dataString := string(data)
+
+    sliceOfStrings := strings.Split(strings.Trim(dataString, "\n"), "\n")
+
+    totalScore := 0
+
+    for _, bout := range sliceOfStrings {
+        totalScore += scoreMap[bout]
+    }
+    fmt.Printf("\n The projected score is: %d", totalScore)
 }
