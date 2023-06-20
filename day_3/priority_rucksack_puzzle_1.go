8cf46af4abe8849d5932e375182b987ca42c59b0
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-06-19 19:33:13 -0700

    first hack at day_3 puzzle, getting incorrect value but code seems right

---
+++ day_3/priority_rucksack_puzzle_1.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+    "os"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func makePrioMaps() (map[rune]int) {
+
+    prioMap := make(map[rune]int)
+    
+    x := 'a'
+    y := 1
+
+    for y <= 26 {
+        prioMap[x] = y
+        x += 1
+        y += 1
+    }
+
+    x = 'A'
+
+    for y <= 52 {
+        prioMap[x] = y
+        x += 1
+        y += 1
+    }
+
+    return prioMap
+}
+
+func main() {
+
+    prioMap := makePrioMaps()
+    returnSum := 0
+    data, err := os.ReadFile("puzzle_input.txt")
+    check(err)
+
+    dataString := string(data)
+
+    rucksackSlice := strings.Split(strings.Trim(dataString, "\n"), "\n")
+
+    for _, sack := range rucksackSlice {
+        firstCompartmentContents := make(map[rune]bool)
+        for i, char := range sack {
+            if i < len(sack)/2 {
+                firstCompartmentContents[char] = true
+            } else {
+                if firstCompartmentContents[char] {
+                    returnSum += prioMap[char]
+                }
+            }
+        }
+    }
+
+    fmt.Println(returnSum)
+
+}
