a6a10b9d6e7a6d02e674181c478f158d2508aa94
Jay Reddy <jayreddy@gmail.com>
2023-07-18 10:19:03 -0700

    inits new branch for refactoring puzzle 2 day 3

--- day_3/puzzle_2/main.go
+++ day_3/puzzle_2/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
-	// rr "github.com/jayreddy040-510/advent_of_code_2022_go/day_3/utils"
+
+	rr "github.com/jayreddy040-510/advent_of_code_2022_go/day_3/utils"
 )
 
 func rucksackMapMaker(r string) map[rune]bool {
@@ -19,8 +21,8 @@ func rucksackMapMaker(r string) map[rune]bool {
 }
 
 func main() {
-	// prioMap := rr.MakePrioMaps()
-	retArray := make([]string, 100)
+	prioMap := rr.MakePrioMaps()
+	retArray := make([]rune, 0)
 
 	f, err := os.Open("../puzzle_input.txt")
 	if err != nil {
@@ -32,7 +34,7 @@ func main() {
 
 	count := 0
 
-	var mapOne, mapTwo map[rune]bool = make(map[rune]bool), make(map[rune]bool)
+	var mapOne map[rune]bool = make(map[rune]bool)
 
 	for scanner.Scan() {
 		text := scanner.Text()
@@ -40,30 +42,30 @@ func main() {
 			mapOne = rucksackMapMaker(text)
 		} else if count == 1 {
 			for _, char := range text {
-				if mapOne != nil {
-					if mapOne[char] {
-						mapTwo[char] = true
-					}
-				} 
+				if !mapOne[char] {
+					delete(mapOne, char)
+				}
 			}
 		} else if count == 2 {
 			for _, char := range text {
-				if mapTwo != nil {
-					if mapTwo[char] {
-						retArray = append(retArray, string(char))
-					}
+				if mapOne[char] {
+					retArray = append(retArray, char)
 				}
 			}
-			mapOne, mapTwo = make(map[rune]bool), make(map[rune]bool)
-			count = 0
+			// mapOne = make(map[rune]bool)
+			count = -1
 		}
 		count++
 	}
 
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
 	returnSum := 0
 
-	// for _, char := range retArray {
-	// 	returnSum += prioMap[byte(char)]
-	// }
-	fmt.Println(retArray,returnSum)
+	for _, char := range retArray {
+		returnSum += prioMap[char]
+	}
+	fmt.Println(retArray, returnSum)
 }
