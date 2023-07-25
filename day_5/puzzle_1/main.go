9fd7fcec95067222b456c87b1644610414767a4a
Jay Reddy <jayreddy@gmail.com>
2023-07-24 17:15:56 -0700

    finishes moveHandler helper func

--- day_5/puzzle_1/main.go
+++ day_5/puzzle_1/main.go
@@ -3,14 +3,28 @@ package main
 import (
 	"os"
 	"fmt"
+	"log"
 	"bufio"
 	"strings"
 	"strconv"
 )
 
-func moveHandler(move string, cratesPointer *map[string][]string) {
+func moveHandler(move string, cratesPointer map[string][]string) {
 	moveSentenceArray := strings.Split(move, " ")
-	moveNum := moveSentenceArray[1]
+	moveNum, err := strconv.Atoi(moveSentenceArray[1])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	from := moveSentenceArray[3]
 	to := moveSentenceArray[5]
+
+	temp := cratesPointer[from][len(cratesPointer[from]) - moveNum : len(cratesPointer[from])]
+	cratesPointer[from] = cratesPointer[from][:len(cratesPointer[from]) - moveNum]
+	cratesPointer[to] = append(cratesPointer[to], temp...)
+}
+
+func main() {
+	
 }
