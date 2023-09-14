456081b27371fed1eb902ce528a6ddbb109b6ccc
Jay Reddy <jayreddy@gmail.com>
2023-09-13 22:22:15 -0700

    restart init

--- day_1/puzzle_1/main.go
+++ day_1/puzzle_1/main.go
@@ -1,57 +1,7 @@
 package main
 
-import (
-	"fmt"
-    "os"
-	"strconv"
-	"strings"
-)
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func maxGroupSum(filename string) int {
-	data, err := os.ReadFile(filename)
-	check(err)
-
-	// Convert byte slice to string
-	dataString := string(data)
-
-	// Split the string into groups
-	groups := strings.Split(dataString, "\n\n")
-
-	// Initialize maximum sum
-	maxSum := 0
-
-	// Iterate over the groups
-	for _, group := range groups {
-		// Split the group into numbers
-		numbers := strings.Split(strings.Trim(group, "\n"), "\n")
-
-		// Initialize group sum
-		groupSum := 0
-
-		// Iterate over the numbers and add them to the group sum
-		for _, numStr := range numbers {
-			num, err := strconv.Atoi(numStr)
-			check(err)
-			groupSum += num
-		}
-
-		// Update maximum sum if necessary
-		if groupSum > maxSum {
-			maxSum = groupSum
-		}
-	}
-
-	return maxSum
-}
+import "fmt"
 
 func main() {
-	filename := "../elves_and_their_calories.txt"
-	fmt.Printf("The maximum group sum is %d\n", maxGroupSum(filename))
-}
 
+}
