e318485c23b6760f78804a301de327011d7bc375
Jay Reddy <jayreddy@gmail.com>
2023-09-14 23:56:45 -0700

    wip

--- day_1/puzzle_1/main.go
+++ day_1/puzzle_1/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
+	f, err := os.ReadFile("../elves_and_their_calories.txt")
+	if err != nil {
+		fmt.Println(fmt.Errorf("error reading file: %v", err))
+	}
 
+	fmt.Println(string(f))
+	fmt.Println("--------------------------------------------")
+	fmt.Println(strings.Split(string(f), "\n\n"))
+	// s := bufio.NewScanner(f)
+
+	// for s.Scan() {
+	// 	fmt.Println(s.Text())
+	// }
 }
