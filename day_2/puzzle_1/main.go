07efde091602d00cbd1d0217c85f8605b3f9e5e0
Jay Reddy <jayreddy@gmail.com>
2023-10-06 17:53:59 -0700

    refactors to lookup table

--- day_2/puzzle_1/main.go
+++ day_2/puzzle_1/main.go
@@ -6,54 +6,46 @@ import (
 	"os"
 )
 
-func rpsHandler(rps string) int {
-	var pts int
-	me, opp := rps[2], rps[0]
+// dont modify willy-nilly
+var lookupTable = map[string]int{
+    "A X": 4,
+    "C X": 7,
+    "B X": 1,
+    "A Y": 8,
+    "B Y": 5,
+    "C Y": 2,
+    "A Z": 3,
+    "B Z": 9,
+    "C Z": 6,
+}
 
-	if me == 'X' {
-		pts += 1
-		if opp == 'A' {
-			pts += 3
-		} else if opp == 'C' {
-			pts += 6
-		}
-	} else if me == 'Y' {
-		pts += 2
-		if opp == 'A' {
-			pts += 6
-		} else if opp == 'B' {
-			pts += 3
-		}
-	} else if me == 'Z' {
-		pts += 3
-		if opp == 'B' {
-			pts += 6
-		} else if opp == 'C' {
-			pts += 3
-		}
-	}
-	return pts
+func rpsHandler (rps string) int {
+    pts, exists := lookupTable[rps]; if exists {
+        return pts
+    } else {
+        return 0
+    }
 }
 
 func main() {
-	file, err := os.Open("../puzzle_input.txt")
-	if err != nil {
-		log.Fatalf("Error reading puzzle input file: %v", err)
-	}
-	defer file.Close()
+    file, err := os.Open("../puzzle_input.txt")
+    if err != nil {
+        log.Fatalf("Error reading puzzle input file: %v", err)
+    }
+    defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	var totalPts int
+    scanner := bufio.NewScanner(file)
+    var totalPts int
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		totalPts += rpsHandler(line)
-	}
+    for scanner.Scan() {
+        line := scanner.Text()
+        totalPts += rpsHandler(line)
+    }
 
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("Error scanning puzzle input: %v", err)
-	}
+    if err := scanner.Err(); err != nil {
+        log.Fatalf("Error scanning puzzle input: %v", err)
+    }
 
-	log.Println(totalPts)
+    log.Println(totalPts)
 
 }
