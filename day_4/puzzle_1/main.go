9b4a8c56e5432d9b82f08b38e2f41d949a11c151
Jay Reddy <jayreddy@gmail.com>
2023-10-07 16:47:38 -0700

    finish puzzle 1 day 4

--- day_4/puzzle_1/main.go
+++ day_4/puzzle_1/main.go
@@ -5,12 +5,56 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func isOverlap(elves string) bool {
-
+func isOverlap(elves string) (bool, error) {
+	twoElvesGroup := strings.Split(elves, ",")
+	elfOne := strings.Split(twoElvesGroup[0], "-")
+	elfTwo := strings.Split(twoElvesGroup[1], "-")
+	elfOneLower, err := strconv.Atoi(elfOne[0])
+	if err != nil {
+		return false, fmt.Errorf("Error converting elf sections: %s into numbers: %v", elves, err)
+	}
+	elfOneUpper, err := strconv.Atoi(elfOne[1])
+	if err != nil {
+		return false, fmt.Errorf("Error converting elf sections: %s into numbers: %v", elves, err)
+	}
+	elfTwoLower, err := strconv.Atoi(elfTwo[0])
+	if err != nil {
+		return false, fmt.Errorf("Error converting elf sections: %s into numbers: %v", elves, err)
+	}
+	elfTwoUpper, err := strconv.Atoi(elfTwo[1])
+	if err != nil {
+		return false, fmt.Errorf("Error converting elf sections: %s into numbers: %v", elves, err)
+	}
+	return (elfOneLower <= elfTwoLower && elfOneUpper >= elfTwoUpper) || (elfTwoLower <= elfOneLower && elfTwoUpper >= elfOneUpper), nil
 }
 
 func main() {
+	file, err := os.Open("../puzzle_input.txt")
+	if err != nil {
+		log.Fatalf("Error opening puzzle input file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var countOverlaps int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if overlappingSection, err := isOverlap(line); overlappingSection && err == nil {
+			countOverlaps++
+		} else if err != nil {
+			log.Fatalf("%v", err)
+		}
+	}
+
+	if scanner.Err() != nil {
+		log.Fatalf("Error scanning puzzle input: %v", err)
+	}
+
+	log.Printf("Num overlaps: %d", countOverlaps)
 
 }
