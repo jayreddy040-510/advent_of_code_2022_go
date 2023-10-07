d9f8d15e4e213a27f2d6d3de360813354c21668c
Jay Reddy <jayreddy@gmail.com>
2023-10-06 17:07:46 -0700

    finishes puzzle_2 of day 1 withotu consulting documentation

--- day_1/puzzle_2/main.go
+++ day_1/puzzle_2/main.go
@@ -1,60 +1,53 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
+    "os"
+    "bufio"
+    "strconv"
+    "log"
 )
 
-func updateMaxSums(maxSums *[]int, sum int) {
-	if sum > (*maxSums)[2] {
-		(*maxSums)[2] = sum
-		if (*maxSums)[2] > (*maxSums)[1] {
-			(*maxSums)[2], (*maxSums)[1] = (*maxSums)[1], (*maxSums)[2]
-			if (*maxSums)[1] > (*maxSums)[0] {
-				(*maxSums)[1], (*maxSums)[0] = (*maxSums)[0], (*maxSums)[1]
-			}
-		}
-	}
+func updateTopThree(topThree *[3]int, num int) {
+    if num > topThree[0] {
+        topThree[2] = topThree[1]
+        topThree[1] = topThree[0]
+        topThree[0] = num
+    } else if num > topThree[1] {
+        topThree[2] = topThree[1]
+        topThree[1] = num
+    } else if num > topThree[2] {
+        topThree[2] = num
+    }
 }
 
 func main() {
-	f, err := os.Open("../elves_and_their_calories.txt")
-	if err != nil {
-		fmt.Printf("error opening file: %s\n", err)
-		return
-	}
-	defer f.Close()
-
-	var sum int
-	maxSums := []int{0, 0, 0}
-
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		if line == "" {
-			updateMaxSums(&maxSums, sum)
-			sum = 0
-			continue
-		}
-
-		num, err := strconv.Atoi(strings.TrimSpace(line))
-		if err != nil {
-			fmt.Printf("error converting string to int: %v\n", err)
-			continue
-		}
-		sum += num
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("error reading file: %s\n", err)
-		return
-	}
-
-	updateMaxSums(&maxSums, sum)
-
-	fmt.Println(maxSums[0] + maxSums[1] + maxSums[2])
+    file, err := os.Open("../elves_and_their_calories.txt")
+    if err != nil {
+        log.Fatalf("Error opening puzzle input: %v", err)
+    }
+    defer file.Close()
+    scanner := bufio.NewScanner(file)
+    var topThree [3]int
+    var count int
+    for scanner.Scan() {
+        line := scanner.Text()
+        if line == "" {
+            updateTopThree(&topThree, count)
+            count = 0
+        } else {
+            lineAsInt, err := strconv.Atoi(line)
+            if err != nil {
+                log.Fatalf("Error processing puzzle input: %v", err)
+            }
+            count += lineAsInt
+        }
+    }
+    // if file doesn't end with newline
+    if count > 0 {
+        updateTopThree(&topThree, count)
+    }
+    if err := scanner.Err(); err != nil {
+        log.Fatalf("Error scanning puzzle input: %v", err)
+    }
+    log.Println(topThree[0] + topThree[1] + topThree[2])
 }
