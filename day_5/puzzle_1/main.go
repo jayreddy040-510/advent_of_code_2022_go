a2d45e7ded3b0cdc735bb92863a0cc2db7634a11
Jay Reddy <jayreddy@gmail.com>
2023-10-19 16:59:21 -0700

    solves stack parsing

--- day_5/puzzle_1/main.go
+++ day_5/puzzle_1/main.go
@@ -1,29 +1,42 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"os"
-	// "regexp"
+	"strings"
+	"regexp"
 )
 
 func main() {
 	f, err := os.Open("../puzzle_input.txt")
 	if err != nil {
 		fmt.Printf("Error opening file: %v", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	content, err := io.ReadAll(f)
+	if err != nil {
+		fmt.Printf("Could not read file: %v", err)
+		os.Exit(1)
+	}
+	fileAsString := string(content)
+
+	parts := strings.Split(fileAsString, "\n\n")
+
+	stacksAsStringUncleaned := parts[0]
+
+	_mid := strings.Split(stacksAsStringUncleaned, "\n")
+
+	stacksAsString := _mid[:len(_mid)-1]
+
+	reg, err := regexp.Compile("[A-Z]")
+	if err != nil {
+		fmt.Printf("Could not compile regex: %v", err)
 	}
 
-	scanner := bufio.NewScanner(f)
-	i := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-        if len(line) == 0 {
-            return
-        }
-		i++
-        if i == 15 {
-            return
-        }
-		fmt.Println(line)
+	for _, line := range stacksAsString {
+		fmt.Println(reg.FindString(line[0:4]))
 	}
 }
